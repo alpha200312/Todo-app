0020aee7739bd23163f9f254357f395d65354949
omkar gadhave <Oggadhave123@gmail.com>
2024-10-21 23:28:16 +0530

    deploy

--- routes/user.js
+++ routes/user.js
@@ -5,14 +5,85 @@ const bcryptjs=require("bcryptjs");
 
 const user_jwt=require('../middleware/user_jwt');
 const jwt=require('jsonwebtoken');
+router.post('/login', async(req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+
+        let user = await User.findOne({
+            email: email
+        });
+
+        if(!user) {
+            return res.status(400).json({
+                success: false,
+                msg: 'User not exists go & register to continue.'
+            });
+        }
+
+
+        const isMatch = await bcryptjs.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({
+                success: false,
+                msg: 'Invalid password'
+            });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, process.env.jwtUserSecret,
+            {
+                expiresIn: 360000
+            }, (err, token) => {
+                if(err) throw err;
+
+                res.status(200).json({
+                    success: true,
+                    msg: 'User logged in',
+                    token: token,
+                    user: user
+                });
+            }
+        )
+
+    } catch(error) {
+        console.log(error.message);
+        res.status(500).json({
+            success: false,
+            msg: 'Server Error'
+        })
+    }
+});
 
 router.get('/',user_jwt,async (req,res,next)=>{
+   try {
     const id=req.user.id;
-    const user= await User.findById(id);
+    const user= await User.findById(id).select('-password');
     console.log(user);
+    res.status(200).json({
+        success:true,
+        user:user
+    })
     console.log("printed");
-    next();
     
+   } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+        success:false,
+        msg:'server error'
+    })
+   }
+   next();
+   
+
     
     
 })
